5072f0f489d5a6d09ddb0109806bab999f7374c4
OjasJain777 <ojasjain777@gmail.com>
2026-01-05 11:18:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort a stack/sort-a-stack.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    void sortStack(stack<int> &st) {
+        vector<int> a;
+        while(!st.empty()){
+            a.push_back(st.top());
+            st.pop();
+        }
+        sort(a.begin(),a.end());
+        for(int i = 0;i<a.size();i++){
+            st.push(a[i]);
+        }
+    }
+};
