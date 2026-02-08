d13d13862d6649e9052560af25200613459e1fed
OjasJain777 <ojasjain777@gmail.com>
2026-02-08 07:56:03 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (76.78%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -9,36 +9,20 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-void t(vector<int> &ans, TreeNode *a, int &i){
+void t(TreeNode *a, int &sum){
     if(a==0){
         return;
     }
-    t(ans, a->left, i);
-    ans.push_back(a->val);
-    a->val = i;
-    i++;
-    t(ans, a->right,i);
-}
-void h(TreeNode *a, vector<int> &s){
-    if(a==0){
-        return;
-    }
-    a->val = s[a->val];
-    h(a->left, s);
-    h(a->right, s);
+    t(a->right, sum);
+    sum += a->val;
+    a->val= sum;
+    t(a->left,sum);
 }
 class Solution {
 public:
     TreeNode* bstToGst(TreeNode* root) {
-        vector<int> ans;
-        int i =0;
-        t(ans, root, i);
-        int sum = 0;
-        for(int j = ans.size()-1;j>=0;j--){
-            sum += ans[j];
-            ans[j] = sum;
-        }
-        h(root, ans);
+        int sum =0;
+        t(root, sum);
         return root;
     }
 };
