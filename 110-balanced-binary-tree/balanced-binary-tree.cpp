15881071b24095e508f21b39207f68d066a0e352
OjasJain777 <ojasjain777@gmail.com>
2026-02-26 16:41:26 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (82.23%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -9,29 +9,34 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-int t(TreeNode *a, int &f){
+int t(TreeNode *a){
     if(a==0){
         return 0;
     }
-    else{
-        int p = t(a->left,f);
-        int k = t(a->right,f);
-        int g = p-k;
-        if(g<-1 || g>1){
-            f=0;
-        }
-        return (max(p,k))+1;
+    return max(t(a->left),t(a->right)) +1;
+}
+void d(TreeNode *a, int &i){
+    if(a==0){
+        return;
     }
+    else if(t(a->left)==0 && t(a->right)==0){
+        return;
+    }
+    else if(t(a->left) - t(a->right) <=1 && t(a->left) - t(a->right) >=-1){
+        i *=1;
+    }
+    else{
+        i*=0;
+        return;
+    }
+    d(a->left, i);
+    d(a->right, i);
 }
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        int f =1;
-        t(root, f);
-        if(f==0){
-            return 0;
-        }
-        return 1;
+        int i =1;
+        d(root, i);
+        return i;
     }
 };
