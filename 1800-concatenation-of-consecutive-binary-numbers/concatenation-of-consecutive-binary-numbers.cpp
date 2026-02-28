7689c3874b3e9952dd9f1e17c2a2e24a2c57952f
OjasJain777 <ojasjain777@gmail.com>
2026-02-28 11:00:55 +0530

    Time: 368 ms (20.81%) | Memory: 8.5 MB (16.11%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,46 @@
+void adder(vector<int> &a, long long &ans){
+    int j =0;
+    long long MOD = 1e9 + 7;
+    int c = 1;
+    while(1){
+        if(j==a.size()){
+            if(c==1){
+                a.push_back(c);
+                break;
+            }
+        }
+        a[j] = a[j]+c;
+        c = a[j]/2;
+        a[j] = a[j]%2;
+        if(c==0){
+            break;
+        }
+        j++;
+    }
+    for (int k = a.size() - 1; k >= 0; k--) {
+        ans = (ans * 2 + a[k]) % MOD;
+    }
+}
+int t(vector<int> bitrack){
+    int j = 0;
+    int ans = 0;
+    while(j<bitrack.size()){
+        ans *= 2;
+        ans += bitrack[j];
+        j++;
+    }
+    return ans;
+}
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        int i =1;
+        long long ans = 0;
+        vector<int> a;
+        while(i<=n){
+            adder(a, ans);
+            i++;
+        }
+        return ans;
+    }
+};
