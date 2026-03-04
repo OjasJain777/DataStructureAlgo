229f2a9e2828d5df1c06fe9fc0342acc6988c9d7
OjasJain777 <ojasjain777@gmail.com>
2026-03-04 12:19:57 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (20.57%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
-        unordered_map<int, int> r;
-        unordered_map<int, int> c;
-        for(int i =0; i<mat.size();i++){
-            for(int j =0; j< mat[0].size();j++){
+        int rs= mat.size();
+        int cs = mat[0].size();
+        vector<int> r(rs, 0);
+        vector<int> c(cs,0);
+        for(int i =0; i<rs;i++){
+            for(int j =0; j<cs;j++){
                 r[i] += mat[i][j];
                 c[j] += mat[i][j];
             }
         }
         int ans=0;
-        for(int i = 0; i<mat.size();i++){
-            for(int j =0;j<mat[0].size();j++){
-                if(mat[i][j] == 1 && r[i]==1 && c[j]==1){
+        for(int i =0; i<rs;i++){
+            for(int j =0; j<cs;j++){
+                if(mat[i][j] ==1 && r[i] == 1 && c[j] == 1){
                     ans++;
                 }
             }
