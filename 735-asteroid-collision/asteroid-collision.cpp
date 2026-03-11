51d8c4edb1a7a8ed3553c3f65db5c942fc4d616c
OjasJain777 <ojasjain777@gmail.com>
2026-03-11 11:53:04 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (77.97%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> ans;
+        for(int i =0; i<asteroids.size();i++){
+            if(asteroids[i]>=0){
+                ans.push_back(asteroids[i]);
+            }
+            else{
+                while(ans.size()>0 && ans[ans.size()-1]>=0 && ans[ans.size()-1] < (asteroids[i]*(-1))){
+                    ans.pop_back();
+                }
+                if(ans.size()==0 || ans[ans.size()-1] <0){
+                    ans.push_back(asteroids[i]);
+                }
+                else if(ans[ans.size()-1]== (asteroids[i]*(-1))){
+                    ans.pop_back();
+                }
+            }
+        }
+        return ans;
+    }
+};
