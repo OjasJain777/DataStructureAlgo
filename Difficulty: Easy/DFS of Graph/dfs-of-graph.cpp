7549788ebb852edf7658112763e39850dcf511e7
OjasJain777 <ojasjain777@gmail.com>
2026-03-16 16:24:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -1,27 +1,25 @@
 class Solution {
   public:
     vector<int> dfs(vector<vector<int>>& adj) {
+        stack<int> a;
+        vector<bool> v(adj.size(),0);
         vector<int> ans = {0};
-        vector<int> a = {0};
-        vector<bool> b(adj.size(),0);
-        b[0] = 1;
-        int i = 0;
-        while(a.size()!=0){
-            if(b[i] == 0){
-                a.push_back(i);
-                ans.push_back(i);
-                b[i]=1;
+        a.push(0);
+        v[0]=1;
+        while(a.size()>0){
+            int c = a.top();
+            int h = 0;
+            for(int x: adj[c]){
+                if(v[x]==0){
+                    a.push(x);
+                    v[x]=1;
+                    ans.push_back(x);
+                    h = 1;
+                    break;
+                }
             }
-            int j =0;
-            while(j<adj[i].size() && b[adj[i][j]] == 1){
-                j++;
-            }
-            if(j<adj[i].size()){
-                i = adj[i][j];
-            }
-            else{
-                a.pop_back();
-                i = a[a.size()-1];
+            if(h==0){
+                a.pop();
             }
         }
         return ans;
