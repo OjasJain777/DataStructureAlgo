223c35bfcc21ae3d2893f56db5648c920e1e5b0b
OjasJain777 <ojasjain777@gmail.com>
2026-03-26 17:48:39 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (32.12%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -36,25 +36,10 @@ public:
                 }
             }
         }
-        for(int r =0; r<board.size();r++){
+        for(int r=0; r<board.size(); r++){
             for(int c =0; c<board[0].size();c++){
                 if(board[r][c]=='O'){
-                    q.push({r,c});
                     board[r][c]='X';
-                }
-            }
-        }
-        while(q.size()>0){
-            auto y = q.front();
-            int i = y.first;
-            int j = y.second;
-            vector<int> dx = {1, -1, 0, 0};
-            vector<int> dy  {0, 0, 1, -1};
-            q.pop();
-            for(int k =0; k<dx.size();k++){
-                if(i+dx[k]>=0 && i+dx[k] < board.size() && j+dy[k] >=0 && j+dy[k] < board[0].size() && board[i+dx[k]][j+dy[k]]=='O'){
-                    q.push({i+dx[k], j+dy[k]});
-                    board[i+dx[k]][j+dy[k]] = 'X';
                 }
             }
         }
