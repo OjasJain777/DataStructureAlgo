707c3b7edcd79306e9dffa3a443bc9897885ff9a
OjasJain777 <ojasjain777@gmail.com>
2026-03-26 07:36:05 +0530

    Time: 29 ms (41.84%) | Memory: 23.3 MB (25.18%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,32 +1,42 @@
-void bfs(vector<vector<char>> &grid, vector<vector<bool>> &v, queue<pair<int,int>> &q){
-    while(q.size()>0){
-        auto y = q.front();
-        q.pop();
-        int i = y.first;
-        int j = y.second;        
-        vector<int> dx = {1 ,-1, 0, 0};
-        vector<int> dy = {0, 0, 1, -1};
-        for(int k =0; k<dx.size(); k++){
-            if( i+ dx[k] < grid.size() && i+dx[k]>=0 && j + dy[k] < grid[0].size() && j+dy[k] >=0 && grid[i+dx[k]][j+dy[k]]=='1' && v[i+dx[k]][j+dy[k]]==0){
-                q.push({i+dx[k], j+dy[k]});
-                v[i+dx[k]][j+dy[k]] = 1;
+class Solution {
+public:
+    void bfs(vector<vector<char>>& grid, int r, int c) {
+        int m = grid.size();
+        int n = grid[0].size();
+        queue<pair<int, int>> q;
+        
+        q.push({r, c});
+        grid[r][c] = '0'; // Mark as visited immediately
+
+        int dx[] = {1, -1, 0, 0};
+        int dy[] = {0, 0, 1, -1};
+
+        while (!q.empty()) {
+            auto [currI, currJ] = q.front();
+            q.pop();
+
+            for (int k = 0; k < 4; k++) {
+                int ni = currI + dx[k];
+                int nj = currJ + dy[k];
+
+                // Check boundaries and if it's land
+                if (ni >= 0 && ni < m && nj >= 0 && nj < n && grid[ni][nj] == '1') {
+                    grid[ni][nj] = '0'; // Sink the land so we don't visit it again
+                    q.push({ni, nj});
+                }
             }
         }
     }
-}
-class Solution {
-public:
+
     int numIslands(vector<vector<char>>& grid) {
-        vector<vector<bool>> v(grid.size(), vector<bool>(grid[0].size()));
-        int t =0;
-        for(int i =0; i<grid.size(); i++){
-            for(int j =0; j<grid[0].size(); j++){
-                if(grid[i][j]=='1' && v[i][j]==0){
+        if (grid.empty()) return 0;
+        int t = 0;
+        
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == '1') {
                     t++;
-                    queue<pair<int, int>> q;
-                    q.push({i,j});
-                    v[i][j]=1;
-                    bfs(grid, v, q);
+                    bfs(grid, i, j);
                 }
             }
         }
