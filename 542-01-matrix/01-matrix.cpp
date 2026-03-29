a9e477629ed75706fd57fb81882f2696616b6e0a
OjasJain777 <ojasjain777@gmail.com>
2026-03-29 11:56:52 +0530

    Time: 14 ms (74.31%) | Memory: 34.8 MB (72.96%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        queue<pair<int,int>> q;
-        vector<int> dx = {1,-1, 0, 0};
-        vector<int> dy = {0, 0, 1, -1};
-        int t =0;
-        vector<vector<bool>> v(mat.size(), vector<bool>(mat[0].size(),0));
-        for(int i =0; i<mat.size();i++){
-            for(int j =0; j<mat[0].size();j++){
-                if(mat[i][j]==0){
-                    q.push({i,j});
-                    v[i][j]=1;
+        int R = mat.size(), C = mat[0].size();
+        queue<pair<int, int>> q;
+        
+        // Using mat itself to track visited: 0 stays 0, 1 becomes -1 (unvisited)
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                if (mat[r][c] == 0) {
+                    q.push({r, c});
+                } else {
+                    mat[r][c] = -1; // Mark as unvisited
                 }
             }
         }
-        while(q.size()>0){
-            t++;
-            int n = q.size();
-            for(int k=0;k<n;k++){
-                auto y = q.front();
-                q.pop();
-                int r= y.first;
-                int c= y.second;
-                for(int m =0; m<dx.size();m++){
-                    if(r+dx[m] >=0 && r+dx[m] < mat.size() && c + dy[m]>=0 && c+dy[m] < mat[0].size()){
-                        if(v[r+dx[m]][c+dy[m]]==0){
-                            q.push({r+dx[m], c+dy[m]});
-                        }
-                        if(mat[r+dx[m]][c+dy[m]]==1 && v[r+dx[m]][c+dy[m]]==0){
-                            mat[r+dx[m]][c+dy[m]] = t;
-                            v[r+dx[m]][c+dy[m]]=1;
-                        }
-                        else if(v[r+dx[m]][c+dy[m]]==0){
-                            v[r+dx[m]][c+dy[m]]=1;
-                        }
-                    }
+
+        int dx[] = {1, -1, 0, 0}, dy[] = {0, 0, 1, -1};
+
+        while (!q.empty()) {
+            auto [r, c] = q.front(); 
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dx[i], nc = c + dy[i];
+
+                // If within bounds AND unvisited (-1)
+                if (nr >= 0 && nr < R && nc >= 0 && nc < C && mat[nr][nc] == -1) {
+                    mat[nr][nc] = mat[r][c] + 1; // Distance = Parent distance + 1
+                    q.push({nr, nc});
                 }
-            }           
+            }
         }
         return mat;
     }
