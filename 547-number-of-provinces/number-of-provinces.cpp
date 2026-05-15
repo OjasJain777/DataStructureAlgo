20ca7724d07864ced9a032e1351815fbb1d39519
OjasJain777 <ojasjain777@gmail.com>
2026-05-15 16:23:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (99.76%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,37 +1,23 @@
-void bfs(vector<vector<int>> &adj, vector<bool> &v, int i, queue<int> &q){
-    while(q.size()>0){
-        int c = q.front();
-        q.pop();
-        for(int x: adj[c]){
-            if(v[x]==0){
-                q.push(x);
-                v[x]=1;
-            }
+void t(vector<vector<int>> &m, vector<bool> &v, int i){
+    v[i]=1;
+    for(int j =0; j<m[0].size();j++){
+        if(m[i][j]==1 && v[j]==0){
+            t(m, v, j);
         }
     }
+    return;
 }
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        vector<vector<int>> adj(isConnected.size());
-        for(int i =0; i<isConnected.size();i++){
-            for(int j =0; j< isConnected[0].size();j++){
-                if(isConnected[i][j]==1){
-                    adj[i].push_back(j);
-                }
+        int c =0;
+        vector<bool> v(isConnected.size(),0);
+        for(int k =0; k<v.size();k++){
+            if(v[k]==0){
+                t(isConnected, v, k);
+                c++;
             }
         }
-        int t= 0;
-        vector<bool> v(adj.size(), 0);
-        for(int i =0; i<adj.size();i++){
-            if(v[i]==0){
-                t++;
-                queue<int> q;
-                q.push(i);
-                v[i]=1;
-                bfs(adj, v,i, q);
-            }
-        }
-        return t;
+        return c;
     }
 };
