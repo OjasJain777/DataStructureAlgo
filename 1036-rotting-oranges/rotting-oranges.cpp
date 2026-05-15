fbd6e5b81732ea5c055b864cf61416390ff19d09
OjasJain777 <ojasjain777@gmail.com>
2026-05-15 16:55:23 +0530

    Time: 4 ms (15.47%) | Memory: 16.6 MB (97.10%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,71 +1,55 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int, int>> q1;
-        vector<vector<bool>> v(grid.size(), vector<bool>(grid[0].size(), 0));
-        int t = 0;
-
-        // 1. Initial scan: Find all rotten oranges
-        for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); j++){
-                if(grid[i][j] == 2){
-                    q1.push({i, j});
-                    v[i][j] = 1;
+        int t =-1;
+        int s = 0;
+        queue<pair<int,int>> q;
+        for(int i =0; i<grid.size();i++){
+            for(int j =0; j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                    s++;
+                }
+                else if(grid[i][j]==2){
+                    q.push({i,j});
                 }
             }
         }
-
-        // 2. BFS with the 'n' snapshot trick
-        while(q1.size() > 0){
-            int n = q1.size(); // Current minute ke oranges ka snapshot
-            bool addedNew = false; // Check if we rot any new oranges this minute
-
-            for(int i = 0; i < n; i++){
-                auto h = q1.front();
-                q1.pop();
-                int r = h.first;
-                int c = h.second;
-
-                // Down
-                if(r+1 < grid.size() && grid[r+1][c] == 1 && v[r+1][c] == 0){
-                    grid[r+1][c] = 2;
-                    v[r+1][c] = 1;
-                    q1.push({r+1, c});
-                    addedNew = true;
+        if(s==0){
+            return 0;
+        }
+        while(q.size()!=0){
+            t++;
+            int n = q.size();
+            for(int k =0; k<n;k++){
+                auto y = q.front();
+                q.pop();
+                int r = y.first;
+                int c = y.second;
+                if(r+1<grid.size() && grid[r+1][c]==1){
+                    grid[r+1][c]=2;
+                    q.push({r+1,c});
+                    s--;
                 }
-                // Up
-                if(r-1 >= 0 && grid[r-1][c] == 1 && v[r-1][c] == 0){
-                    grid[r-1][c] = 2;
-                    v[r-1][c] = 1;
-                    q1.push({r-1, c});
-                    addedNew = true;
+                if(r-1>=0 && grid[r-1][c]==1){
+                    grid[r-1][c]=2;
+                    q.push({r-1,c});
+                    s--;
                 }
-                // Right
-                if(c+1 < grid[0].size() && grid[r][c+1] == 1 && v[r][c+1] == 0){
-                    grid[r][c+1] = 2;
-                    v[r][c+1] = 1;
-                    q1.push({r, c+1});
-                    addedNew = true;
+                if(c+1< grid[0].size() && grid[r][c+1] ==1){
+                    grid[r][c+1]= 2;
+                    q.push({r,c+1});
+                    s--;
                 }
-                // Left
-                if(c-1 >= 0 && grid[r][c-1] == 1 && v[r][c-1] == 0){
-                    grid[r][c-1] = 2;
-                    v[r][c-1] = 1;
-                    q1.push({r, c-1});
-                    addedNew = true;
+                if(c-1>=0 && grid[r][c-1]==1){
+                    grid[r][c-1]=2;
+                    q.push({r, c-1});
+                    s--;
                 }
             }
-            
-            if(addedNew) t++; // Only increment time if new oranges were rotted
         }
-
-        // 3. Final Check: Any fresh oranges left?
-        for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); j++){
-                if(grid[i][j] == 1) return -1;
-            }
+        if(s>0){
+            return -1;
         }
-
         return t;
     }
 };
