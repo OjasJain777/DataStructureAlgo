0be96c545a0e7dc1a8ea212df820e83c60eaa3a6
OjasJain777 <ojasjain777@gmail.com>
2026-05-17 14:35:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -1,27 +1,31 @@
 class Solution {
   public:
     vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
-        vector<int> d(V, INT_MAX);
-        vector<vector<vector<int>>> b(V);
-        for(vector<int> x: edges){
-            b[x[0]].push_back({x[1], x[2]});
-            b[x[1]].push_back({x[0], x[2]});
+        vector<vector<vector<int>>> adj(V);
+        vector<int> ans(V, -1);
+        vector<bool> v(V,0);
+        ans[src]=0;
+        for(auto y: edges){
+            adj[y[0]].push_back({y[1], y[2]});
+            adj[y[1]].push_back({y[0], y[2]});
         }
-        d[src] = 0;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, src});
-        while(pq.size()>0){
-            int u = pq.top().second;
-            int j = pq.top().first;
-            pq.pop();
-            if (j > d[u]) continue;
-            for(vector<int> x: b[u]){
-                if(d[x[0]] > d[u] + x[1]){
-                    d[x[0]] = d[u] + x[1];
-                    pq.push({d[x[0]], x[0]});
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> q;
+        q.push({0, src});
+        while(q.size()!=0){
+            auto z = q.top();
+            int c = z[1];
+            int d = z[0];
+            q.pop();
+            if(v[c]==0){
+                for(auto x: adj[c]){
+                    if(ans[x[0]]==-1 || ans[x[0]]> x[1]+ d){
+                        ans[x[0]]= x[1]+ d;
+                        q.push({ans[x[0]], x[0]});
+                    }
                 }
             }
+            v[c]=1;
         }
-        return d;
+        return ans;
     }
 };
