c5420cc1eadaee6105eaeb1ed3865a002917c837
OjasJain777 <ojasjain777@gmail.com>
2026-05-17 11:31:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,35 +1,33 @@
 class Solution {
   public:
     bool isCycle(int V, vector<vector<int>>& edges) {
-        vector<bool> v(V,0);
         vector<vector<int>> adj(V);
-        stack<pair<int, int>> s;
-        for(int i =0; i<edges.size();i++){
-            adj[edges[i][0]].push_back(edges[i][1]);
-            adj[edges[i][1]].push_back(edges[i][0]);
+        for(auto y: edges){
+            adj[y[0]].push_back(y[1]);
+            adj[y[1]].push_back(y[0]);
         }
-        for(int i =0; i<V; i++){
-            if(v[i]==1){
-                continue;
-            }
-            s.push({i,i});
-            v[i]=1;
-            while(s.size()>0){
-                auto y = s.top();
-                int c = y.first;
-                int f = y.second;
-                if(adj[c].size()==0){
-                    s.pop();
-                    continue;
+        vector<bool> v(V,0);
+        for(int i =0; i<V;i++){
+            if(v[i]==0){
+                queue<pair<int, int>> q;
+                q.push({i,-1});
+                v[i]=1;
+                while(q.size()!=0){
+                    int n = q.size();
+                    for(int k=0; k<n;k++){
+                        auto [c,z] = q.front();
+                        q.pop();
+                        for(auto j:adj[c]){
+                            if(v[j]==1 && j!=z){
+                                return 1;
+                            }
+                            else if (v[j]==0){
+                                q.push({j, c});
+                                v[j]=1;
+                            }
+                        }
+                    }
                 }
-                else if(v[adj[c][adj[c].size()-1]]==0){
-                    s.push({adj[c][adj[c].size()-1], c});
-                    v[adj[c][adj[c].size()-1]]=1;
-                }
-                else if(f != adj[c][adj[c].size()-1]){
-                    return 1;
-                }
-                adj[c].pop_back();
             }
         }
         return 0;
