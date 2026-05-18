68d63d140c06538c4eb8fe1815a003897dfb1397
OjasJain777 <ojasjain777@gmail.com>
2026-05-18 22:09:24 +0530

    Time: 60 ms (99.77%) | Memory: 44.3 MB (67.03%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int> ans(n+1, INT_MAX);
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> q;
-        vector<vector<vector<int>>> adj(n+1);
-        for(auto y: times){
-            adj[y[0]].push_back({y[1],y[2]});
+        // Fast I/O trick jo Leetcode par runtime aadha kar deti hai
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        // 3D vector ki jagah pair use kiya, memory allocation fast ho gayi
+        vector<vector<pair<int, int>>> adj(n + 1);
+        for(const auto& y: times){ // '&' use kiya taaki copy na ho, direct reference mile
+            adj[y[0]].push_back({y[1], y[2]});
         }
-        ans[0]=0;
-        ans[k]=0;
-        q.push({0,k});
-        while(q.size()!=0){
-            auto [d,c] = q.top();
+
+        vector<int> ans(n + 1, INT_MAX);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> q;
+        
+        ans[0] = 0; // Tera mast jugaad wala fix
+        ans[k] = 0;
+        q.push({0, k});
+        
+        while(!q.empty()){
+            auto [d, c] = q.top();
             q.pop();
-            if(d>ans[c]){
-                continue;
-            }
-            for(auto z: adj[c]){
-                if(ans[z[0]] > ans[c] + z[1]){
-                    ans[z[0]] = ans[c] + z[1];
-                    q.push({ans[z[0]], z[0]});
+            
+            if(d > ans[c]) continue;
+            
+            // '&' use kiya taaki loop ke andar har bar pair copy na ho
+            for(const auto& z: adj[c]){
+                int neighbor = z.first;
+                int weight = z.second;
+                
+                if(ans[neighbor] > d + weight){
+                    ans[neighbor] = d + weight;
+                    q.push({ans[neighbor], neighbor});
                 }
             }
         }
+        
         int a = -1;
-        for(int j =0; j<ans.size();j++){
-            if(j==k){
-                continue;
-            }
-            if(ans[j]==INT_MAX){
-                return -1;
-            }
+        for(int j = 0; j < ans.size(); j++){
+            if(j == k) continue;
+            if(ans[j] == INT_MAX) return -1;
             a = max(a, ans[j]);
         }
         return a;
