cf460f3f2d9752b6f1624ec2e63aaac3080c6c1b
OjasJain777 <ojasjain777@gmail.com>
2026-05-22 22:17:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -13,31 +13,27 @@ class Node {
 };
 */
 
-void t(map<int,vector<int>> &b, Node *a, int i, int j){
+void t(Node *a, int l, int v, map<int, pair<int, Node *>> &b){
     if(a==0){
         return;
     }
-    if(b.count(j)==0){
-        b[j] = {i,a->data};
+    if(b.count(v)==0){
+        b[v] = {l, a};
     }
-    else{
-        if(i<b[j][0]){
-            b[j][0]=i;
-            b[j][1]=a->data;
-        }
+    else if(b[v].first > l){
+        b[v] = {l, a};
     }
-    t(b, a->left, i+1, j-1);
-    t(b, a->right, i+1, j+1);
+    t(a->left, l+1, v-1, b);
+    t(a->right, l+1,v+1,b);
 }
-
 class Solution {
   public:
     vector<int> topView(Node *root) {
-        map<int, vector<int>> b;
-        t(b,root, 0,0);
+        map<int, pair<int, Node *>> b;
         vector<int> ans;
-        for(auto y:b){
-            ans.push_back(y.second[1]);
+        t(root, 0,0,b);
+        for(auto y: b){
+            ans.push_back(y.second.second->data);
         }
         return ans;
     }
