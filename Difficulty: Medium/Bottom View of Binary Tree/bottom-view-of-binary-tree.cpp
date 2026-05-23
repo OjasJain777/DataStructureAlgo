86c06c046a2e7be11aaa6e6db47706e098d67753
OjasJain777 <ojasjain777@gmail.com>
2026-05-23 12:00:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.cpp
+++ Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.cpp
@@ -1,39 +1,41 @@
 /*
+Definition for Node
 class Node {
 public:
     int data;
     Node* left;
     Node* right;
 
-    Node(int x) {
-        data = x;
-        left = right = NULL;
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
     }
 };
 */
-void t(map<int, vector<int>> &b, Node *a, int i, int j){
-    if(a==0){
-        return;
-    }
-    if(b.count(j)==0){
-        b[j] = {i, a->data};
-    }
-    else{
-        if(b[j][0] <= i){
-            b[j] = {i, a->data};
-        }
-    }
-    t(b,a->left,i+1,j-1);
-    t(b,a->right,i+1,j+1);
-}
+
 class Solution {
   public:
     vector<int> bottomView(Node *root) {
-        map<int, vector<int>> b;
-        t(b,root, 0,0);
+        map<int, int> b;
         vector<int> ans;
-        for(auto y:b){
-            ans.push_back(y.second[1]);
+        queue<pair<int,Node *>> q;
+        if(root==0){
+            return ans;
+        }
+        q.push({0,root});
+        while(q.size()!=0){
+            auto [v,t]  = q.front();
+            q.pop();
+            b[v] = t->data;
+            if(t->left!=0){
+                q.push({v-1, t->left});
+            }
+            if(t->right !=0){
+                q.push({v+1, t->right});
+            }
+        }
+        for(auto y: b){
+            ans.push_back(y.second);
         }
         return ans;
     }
