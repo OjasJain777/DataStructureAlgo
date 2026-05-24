ea9f1e68a5615c38f77a7fca6ed9473872ae1022
OjasJain777 <ojasjain777@gmail.com>
2026-05-24 15:24:29 +0530

    Time: 10 ms (3.28%) | Memory: 29.5 MB (12.70%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -1,41 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if (!root) return 0;
-        
-        unsigned long long max_width = 0;
-        queue<pair<TreeNode*, unsigned long long>> q;
-        q.push({root, 0}); // Start root at index 0
-
-        while (!q.empty()) {
-            int size = q.size();
-            
-            // Get indices of the first and last node in THIS level
-            unsigned long long start_index = q.front().second;
-            unsigned long long end_index = q.back().second;
-            
-            max_width = max(max_width, end_index - start_index + 1);
-
-            for (int i = 0; i < size; i++) {
-                // Get the node and its index
-                pair<TreeNode*, unsigned long long> p = q.front();
-                TreeNode* node = p.first;
-                unsigned long long idx = p.second;
-                q.pop();
-
-                // Push children with updated indices
-                // To prevent overflow in VERY deep trees, we can subtract 'start_index'
-                // effectively normalizing the level to start at 0.
-                long long normalized_idx = idx - start_index; 
-                
-                if (node->left) {
-                    q.push({node->left, 2 * normalized_idx});
-                }
-                if (node->right) {
-                    q.push({node->right, 2 * normalized_idx + 1});
-                }
+        queue<pair<unsigned long long, TreeNode *>> q;
+        q.push({0,root});
+        int ans =1;
+        queue<pair<unsigned long long, TreeNode *>> q1;
+        unsigned long long  mi = ULLONG_MAX;
+        unsigned long long ma = 0;
+        while(q.size()!=0){
+            auto [v,t] = q.front();
+            q.pop();
+            mi = min(mi, v);
+            ma = max(ma, v);
+            if(t->left!=0){
+                q1.push({2*v,t->left});
+            }
+            if(t->right!=0){
+                q1.push({2*v+1, t->right});
+            }
+            if(q.size()==0){
+                q = q1;
+                q1={};
+                int d = (int)(ma-mi+1);
+                mi = ULLONG_MAX;
+                ma = 0;
+                ans = max(d, ans);
             }
         }
-        return max_width;
+        return ans;
     }
 };
