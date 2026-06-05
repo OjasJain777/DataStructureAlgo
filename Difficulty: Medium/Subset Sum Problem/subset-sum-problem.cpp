359017ec313f4befc0c506aaf95865014c6ab82e
OjasJain777 <ojasjain777@gmail.com>
2026-06-05 21:40:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -1,30 +1,17 @@
-bool t(int k, int s, int r,int &sum, map<pair<int,int>, bool> &dp, vector<int> &arr){
-    if(s>sum){
-        return 0;
-    }
-    if(s+r < sum){
-        return 0;
-    }
-    if(s==sum){
-        return 1;
-    }
-    if(k>= arr.size()){
-        return 0;
-    }
-    if(dp.count({k,s})!=0){
-        return dp[{k,s}];
-    }
-    dp[{k,s}] = t(k+1, s, r-arr[k], sum,dp,arr) ||t(k+1, s+arr[k], r-arr[k], sum, dp,arr);
-    return dp[{k,s}];
-}
 class Solution {
   public:
     bool isSubsetSum(vector<int>& arr, int sum) {
-        int r=0;
-        for(auto x : arr){
-            r+=x;
+        unordered_set<int> s;
+        s.insert(0);
+        for(int i =0; i<arr.size();i++){
+            unordered_set<int> p=s;
+            for(auto y: p){
+                s.insert(y+arr[i]);
+            }
+            if(s.count(sum)==1){
+                return 1;
+            }
         }
-        map<pair<int,int>,bool> dp;
-        return t(0, 0, r, sum, dp, arr);
+        return 0;
     }
 };
