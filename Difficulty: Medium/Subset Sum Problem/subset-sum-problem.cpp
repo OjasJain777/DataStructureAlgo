deb8b7274ae6ebc9fe7066a11515f71d70055a2e
OjasJain777 <ojasjain777@gmail.com>
2026-06-05 22:08:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -1,15 +1,16 @@
 class Solution {
   public:
     bool isSubsetSum(vector<int>& arr, int sum) {
-        unordered_set<int> s;
-        s.insert(0);
-        for(int i =0; i<arr.size();i++){
-            unordered_set<int> p=s;
-            for(auto y: p){
-                s.insert(y+arr[i]);
-            }
-            if(s.count(sum)==1){
-                return 1;
+        vector<bool> s(sum+1, 0);
+        s[0] = 1;
+        for(int i =0;i<arr.size();i++){
+            for(int j = sum; j>=0;j--){
+                if(s[j] && j + arr[i] <= sum){
+                    s[j + arr[i]] = 1;
+                }
+                if(s[s.size()-1]==1){
+                    return 1;
+                }
             }
         }
         return 0;
