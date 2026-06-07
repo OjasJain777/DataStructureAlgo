79f07693235139dea5d742e8d61efff70cd50843
OjasJain777 <ojasjain777@gmail.com>
2026-06-07 07:09:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (54.19%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,31 +1,49 @@
+void t(int k, string &s, int &p, long &ans, int &si){
+    if(k>= s.size()){
+        return;
+    }
+    if((s[k]>='a' && s[k]<='z') || (s[k]<='Z' && s[k]>= 'A') || s[k] == '.'){
+        return;
+    }
+        if(p==1){
+        if(s[k]>='0' && s[k] <='9'){
+            if(si==-1){
+                ans  = ans*10 - (s[k]-'0');
+                ans = max((long)INT_MIN, ans);
+            }
+            else{
+                ans = ans*10 + (s[k]-'0');
+                ans = min(ans, (long)INT_MAX);
+            }
+        }
+        else{
+            return;
+        }
+    }
+    if(p==0){
+        if(s[k] == '-'){
+            si=-1;
+            p++;
+        }
+        else if(s[k]=='+'){
+            si=1;
+            p++;
+        }
+        else if(s[k]<='9' && s[k] >= '0'){
+            p++;
+            t(k, s, p, ans, si);
+            return;
+        }
+    }
+    t(k+1, s, p, ans, si);
+}
 class Solution {
 public:
     int myAtoi(string s) {
-        int i =0;
-        int m = 1;
-        long long ans =0;
-        while(s[i] == ' '){
-            i++;
-        }
-        if(s[i] == '+'){
-            i++;
-        }
-        else if(s[i]=='-'){
-            i++;
-            m*=-1;
-        }
-        while(s[i] >= '0' && s[i] <= '9'){
-            ans *= 10;
-            ans += s[i]-'0';
-            if(ans*m>=INT_MAX){
-                return INT_MAX;
-            }
-            else if(ans*m <= INT_MIN){
-                return INT_MIN;
-            }
-            i++;
-        }
-        ans *= m;
-        return (int)ans;
+        long ans = 0;
+        int p =0;
+        int si = 1;
+        t(0, s, p, ans,si);
+        return ans;
     }
 };
