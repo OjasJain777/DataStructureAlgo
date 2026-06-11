62de19de8c2e5a8894f3812a80a1d0400092308c
OjasJain777 <ojasjain777@gmail.com>
2026-06-11 14:58:01 +0530

    Time: 346 ms (43.83%) | Memory: 10.5 MB (81.37%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,50 +1,35 @@
-void t(int i, int j, string a, vector<vector<char>> &b, int l, int &ans){
-    int c =0;
-    if(l==a.size()){
-        ans += 1;
-        return;
+vector<int> dx = {1,-1,0,0};
+vector<int> dy = {0,0,1,-1};
+bool t(int i, int j, vector<vector<char>> &board, string &a, string &word){
+    if(a.size()==word.size()){
+        return 1;
     }
-    if(i>0 && b[i-1][j] == a[l]){
-        int g = b[i][j];
-        b[i][j] = '%';
-        t(i-1, j, a, b, l+1, ans);
-        b[i][j] = g;
-
-        c=1;
+    bool r = 0;
+    for(int k =0;k<4;k++){
+        if(i+dx[k]>=0 && i+dx[k]<board.size() && j+dy[k]>=0 && j+dy[k]<board[0].size() && board[i+dx[k]][j+dy[k]] == word[a.size()]){
+            a.push_back(word[a.size()]);
+            char h = board[i+dx[k]][j+dy[k]];
+            board[i+dx[k]][j+dy[k]] = '1';
+            r = r || t(i+dx[k], j+dy[k], board, a, word);
+            board[i+dx[k]][j+dy[k]] = h;
+            a.pop_back();
+        }
     }
-    if(i<b.size()-1 && b[i+1][j] == a[l]){
-        int g = b[i][j];
-        b[i][j] = '%';
-        t(i+1, j, a, b, l+1, ans);
-        b[i][j] = g;
-        c=1;
-    }
-    if(j>0 && b[i][j-1] == a[l]){
-        int g = b[i][j];
-        b[i][j] = '%';
-        t(i, j-1, a, b, l+1, ans);
-        b[i][j] = g;
-        c=1;
-    }
-    if(j<b[0].size()-1 && b[i][j+1] == a[l]){
-        int g = b[i][j];
-        b[i][j] = '%';
-        t(i, j+1, a, b, l+1, ans);
-        b[i][j] = g;
-        c=1;
-    }
-    if(c==0){
-        ans += 0;
-    }
+    return r;
 }
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int ans = 0;
-        for(int i =0; i<board.size();i++){
-            for(int j =0; j<board[0].size();j++){
+        string a;
+        bool ans = 0;
+        for(int i = 0;i<board.size();i++){
+            for(int j = 0;j<board[0].size();j++){
                 if(board[i][j] == word[0]){
-                    t(i, j, word, board, 1, ans);
+                    a.push_back(word[0]);
+                    board[i][j]='1';
+                    ans = ans || t(i,j, board, a, word);
+                    board[i][j]=word[0];
+                    a.pop_back();
                 }
             }
         }
