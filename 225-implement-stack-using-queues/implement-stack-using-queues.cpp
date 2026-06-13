2876b52a04538e1b60df171af54a905d717630b2
OjasJain777 <ojasjain777@gmail.com>
2026-06-13 15:54:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.55%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,39 +1,48 @@
 class MyStack {
 public:
     queue<int> q1;
-    queue<int> q2;
     MyStack() {
+        q1={};
     }
     
     void push(int x) {
-        while(!q1.empty()){
-            q2.push(q1.front());
-            q1.pop();
-        }
         q1.push(x);
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
-        }
     }
     
     int pop() {
-        int a = q1.front();
+        if(q1.size()==0){
+            return -1;
+        }
+        for(int i =0;i<q1.size()-1;i++){
+            int a = q1.front();
+            q1.pop();
+            q1.push(a);
+        }
+        auto a = q1.front();
         q1.pop();
-        return a;
+        return a;        
     }
     
     int top() {
-        return q1.front();
+        if(q1.size()==0){
+            return -1;
+        }
+        for(int i =0;i<q1.size()-1;i++){
+            int a = q1.front();
+            q1.pop();
+            q1.push(a);
+        }
+        auto a = q1.front();
+        q1.pop();
+        q1.push(a);
+        return a;                
     }
     
     bool empty() {
-        if(q1.empty()!=0){
-            return true;
+        if(q1.size()==0){
+            return 1;
         }
-        else{
-            return false;
-        }
+        return 0;
     }
 };
 
