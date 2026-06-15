60ff495f354b9bd84a19eb58dbc7d7dca358e20b
OjasJain777 <ojasjain777@gmail.com>
2026-06-15 19:29:03 +0530

    Time: 35 ms (17.37%) | Memory: 87.7 MB (14.93%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        vector<int> a;
-        vector<int> b;
-        vector<int> a1(heights.size(), 0);
-        vector<int> b1(heights.size(),0);
-        for(int i = 0; i<heights.size();i++){
-            while(a.size()>0 && heights[i] <= heights[a[a.size()-1]]){
-                a.pop_back();
+        vector<int> nse(heights.size());
+        vector<int> pse(heights.size());
+        stack<int> s;
+        for(int i = heights.size()-1; i>=0 ; i--){
+            while(s.size()!=0 && heights[i] <= heights[s.top()]){
+                s.pop();
             }
-            if(a.size()==0){
-                a1[i] = -1;
-            }
-            else{
-                a1[i] = a[a.size()-1];
-            }
-            a.push_back(i);
-        }
-        for(int k = heights.size()-1;k>=0;k--){
-            while((b.size()>0 && heights[k] <= heights[b[b.size()-1]])){
-                b.pop_back();
-            }
-            if(b.size()==0){
-                b1[k] = heights.size();
+            if(s.size()==0){
+                nse[i]= heights.size();
             }
             else{
-                b1[k] = b[b.size()-1];
+                nse[i] = s.top();
             }
-            b.push_back(k);
+            s.push(i);
         }
-        int s; 
-        int e;
-        int area = 0;
+        s={};
         for(int i =0; i<heights.size();i++){
-            s = a1[i];
-            e = b1[i];
-            int a = (e-s-1)*(heights[i]);
-            area = max(area,a);
+            while(s.size()!=0 && heights[i] < heights[s.top()]){
+                s.pop();
+            }
+            if(s.size()==0){
+                pse[i] = -1;
+            }
+            else{
+                pse[i] = s.top();
+            }
+            s.push(i);
         }
-        return area;
+        int ans =0;
+        for(int k =0; k<heights.size();k++){
+            ans = max(ans,(heights[k] * (nse[k]-pse[k]-1)));
+        }
+        return ans;
     }
 };
