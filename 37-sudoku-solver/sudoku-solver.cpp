d580f2c59d0aac66b6986734e50e55316c467a0e
OjasJain777 <ojasjain777@gmail.com>
2026-06-15 10:09:18 +0530

    Time: 43 ms (97.29%) | Memory: 8.9 MB (9.83%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,52 +1,69 @@
-bool t(vector<vector<char>> &board, unordered_map<int, unordered_set<char>> &b, unordered_map<int, unordered_set<char>> &c, unordered_map<int, unordered_set<char>> &r, int i, int j){
-    if(i==9){
-        return 1;
-    }
-    if(j==9){
-        if(t(board, b, c,r, i+1,0)){
-            return 1;
-        }
-    }
-    if(board[i][j]<='9' && board[i][j]>='1'){
-        if(t(board, b, c,r,i,j+1)){
-            return 1;
-        }
-    }
-    else{
-        for(int k =1;k<=9;k++){
-            if(b[(i/3)*3 +j/3].count(k+'0')==1 || c[j].count(k+'0')==1 || r[i].count(k+'0')==1){
-                continue;
-            }
-            board[i][j]=k+'0';
-            b[(i/3)*3+j/3].insert(k+'0');
-            c[j].insert(k+'0');
-            r[i].insert(k+'0');
-            if(t(board, b,c,r,i,j+1)){
-                return 1;
-            }
-            b[(i/3)*3+j/3].erase(k+'0');
-            c[j].erase(k+'0');
-            board[i][j] = '.';
-            r[i].erase(k+'0');
-        }
-    }
-    return 0;
-}
 class Solution {
 public:
+
+    int row[9] = {};
+    int col[9] = {};
+    int box[9] = {};
+
+    bool solve(vector<vector<char>>& board, int i, int j) {
+
+        if(i == 9)
+            return true;
+
+        if(j == 9)
+            return solve(board, i + 1, 0);
+
+        if(board[i][j] != '.')
+            return solve(board, i, j + 1);
+
+        int b = (i/3)*3 + (j/3);
+
+        for(int d = 1; d <= 9; d++) {
+
+            int bit = 1 << (d - 1);
+
+            if((row[i] & bit) ||
+               (col[j] & bit) ||
+               (box[b] & bit))
+                continue;
+
+            board[i][j] = d + '0';
+
+            row[i] |= bit;
+            col[j] |= bit;
+            box[b] |= bit;
+
+            if(solve(board, i, j + 1))
+                return true;
+
+            board[i][j] = '.';
+
+            row[i] ^= bit;
+            col[j] ^= bit;
+            box[b] ^= bit;
+        }
+
+        return false;
+    }
+
     void solveSudoku(vector<vector<char>>& board) {
-        unordered_map<int, unordered_set<char>> b;
-        unordered_map<int, unordered_set<char>> c;
-        unordered_map<int, unordered_set<char>> r;
-        for(int i =0;i<board.size();i++){
-            for(int j =0;j<board[0].size();j++){
-                if(board[i][j] >='1' && board[i][j] <='9'){
-                    c[j].insert(board[i][j]);
-                    r[i].insert(board[i][j]);
-                    b[(i/3)*3 + j/3].insert(board[i][j]);
-                }
+
+        for(int i = 0; i < 9; i++) {
+
+            for(int j = 0; j < 9; j++) {
+
+                if(board[i][j] == '.')
+                    continue;
+
+                int d = board[i][j] - '0';
+                int bit = 1 << (d - 1);
+
+                row[i] |= bit;
+                col[j] |= bit;
+                box[(i/3)*3 + (j/3)] |= bit;
             }
         }
-        t(board, b,c,r,0,0);
+
+        solve(board, 0, 0);
     }
 };
