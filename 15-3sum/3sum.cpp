7179c1dabdf5de40a74fcef076e882800f7aae31
OjasJain777 <ojasjain777@gmail.com>
2026-06-24 03:25:15 +0530

    Time: 51 ms (43.99%) | Memory: 29.2 MB (46.73%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,53 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int i =0;
-        int l;
-        vector<vector<int>> ans;
-        int r;
-        while(i<nums.size()-2){
-            int target = (-1)*(nums[i]);
-            if(i==0){
-                l = i+1;
-                r = nums.size()-1;
-            }
-            else if(nums[i]==nums[i-1]){
-                l=0;
-                r=0;
-            }
-            else{
-                l=i+1;
-                r=nums.size()-1;
-            }
-            while(l<r){
-                if(nums[l] + nums[r] == target){
-                    ans.push_back({nums[i],nums[l],nums[r]});
-                    while(l<nums.size()-1 && nums[l+1]==nums[l] ){
-                        l++;
-                    }
-                    while(r>0 && nums[r-1]==nums[r] ){
-                        r--;
-                    }
-                    r--;
-                    l++;
-                }
-                else if(nums[l] + nums[r] > target){
-                    while(r>0 && nums[r-1]==nums[r]){
-                        r--;
-                    }
-                    r--;
-                }
-                else{
-                    while(l<nums.size()-1 && nums[l+1]==nums[l]){
-                        l++;
-                    }
-                    l++;
-                }
+        sort(nums.begin(), nums.end());
 
+        vector<vector<int>> ans;
+        int n = nums.size();
+
+        for(int i = 0; i < n - 2; i++) {
+
+            if(i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int l = i + 1;
+            int r = n - 1;
+
+            while(l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if(sum < 0) {
+                    l++;
+                }
+                else if(sum > 0) {
+                    r--;
+                }
+                else {
+                    ans.push_back({nums[i], nums[l], nums[r]});
+
+                    // Skip duplicates
+                    while(l < r && nums[l] == nums[l + 1]) l++;
+                    while(l < r && nums[r] == nums[r - 1]) r--;
+
+                    l++;
+                    r--;
+                }
             }
-            i++;
         }
+
         return ans;
     }
 };
