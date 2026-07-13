e6e4db0da20350ef93ace4772bfa9a3647e92938
OjasJain777 <ojasjain777@gmail.com>
2026-07-13 14:32:50 +0530

    Time: 43 ms (5.62%) | Memory: 24.6 MB (5.08%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,34 +1,42 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<vector<int>>> b(n);
-        for(vector<int> x: flights){
-            b[x[0]].push_back({x[1],x[2]});
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> q;
+        vector<vector<bool>> v(n,vector<bool>(k+2,0));
+        vector<vector<int>> d(n, vector<int>(k+2, INT_MAX));
+        vector<vector<vector<int>>> adj(n);
+        for(auto &y:flights){
+            adj[y[0]].push_back({y[1],y[2]});
         }
-        vector<int> d(n, INT_MAX);
-        d[src]=0;
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        pq.push({0, 0, src});
-        while(pq.size()>0){
-            int j = pq.top()[2];
-            int u = pq.top()[1];
-            int l = pq.top()[0];
-            pq.pop();
-            if(l >= k+1){
+        d[src][k+1]=0;
+        q.push({0,src, k+1});
+        while(!q.empty()){
+            auto y= q.top();
+            q.pop();
+            int cd = y[0];
+            int cn = y[1];
+            int s = y[2];
+            if(v[cn][s]==1){
                 continue;
             }
-            else{
-                for(vector<int> x: b[j]){
-                    if(d[x[0]] > u + x[1]){
-                        d[x[0]] = u + x[1];
-                        pq.push({l+1, d[x[0]], x[0]});
-                    }
+            v[cn][s] =1;
+            if(s==0){
+                continue;
+            }
+            for(auto &x: adj[cn]){
+                if(v[x[0]][s-1]==0 && d[x[0]][s-1] > cd + x[1]){
+                    d[x[0]][s-1] = cd + x[1];
+                    q.push({d[x[0]][s-1], x[0], s-1});
                 }
             }
         }
-        if(d[dst]==INT_MAX){
+        int ans =INT_MAX;
+        for(int x: d[dst]){
+            ans = min(ans, x);
+        }
+        if(ans==INT_MAX){
             return -1;
         }
-        return d[dst];
+        return ans;
     }
 };
