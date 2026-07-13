e19693e772fbbe7c7ed5d6193875b5744ec1062c
OjasJain777 <ojasjain777@gmail.com>
2026-07-13 18:12:02 +0530

    Time: 10 ms (48.90%) | Memory: 34.9 MB (92.36%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,38 +1,45 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<long long> w(n, 0);
-        long long mod = 1e9 + 7;
-        vector<long long> d(n, LLONG_MAX);
-        d[0] = 0;
-        vector<vector<vector<long long>>> adj(n);
-        for (auto& x : roads) {
-            adj[x[0]].push_back({x[1], x[2]});
-            adj[x[1]].push_back({x[0], x[2]});
+        // Use vector<pair<int, int>> instead of vector<vector<long long>>
+        // adj[u] will store {v, time}
+        vector<vector<pair<int, int>>> adj(n);
+        for (const auto& road : roads) {
+            adj[road[0]].push_back({road[1], road[2]});
+            adj[road[1]].push_back({road[0], road[2]});
         }
-        w[0] = 1;
-        priority_queue<vector<long long>, vector<vector<long long>>,
-                       greater<vector<long long>>>
-            q;
-        q.push({0, 0});
-        while (!q.empty()) {
-            auto y = q.top();
-            long long cd = y[0];
-            long long cn = y[1];
-            q.pop();
-            if (cd > d[cn]) {
-                continue;
-            }
-            for (auto& x : adj[cn]) {
-                if (d[x[0]] > cd + x[1]) {
-                    d[x[0]] = cd + x[1];
-                    w[x[0]] = w[cn];
-                    q.push({d[x[0]], x[0]});
-                } else if (d[x[0]] == cd + x[1]) {
-                    w[x[0]] = (w[x[0]] + w[cn]) % mod;
+
+        vector<long long> dist(n, LLONG_MAX);
+        vector<long long> ways(n, 0);
+        dist[0] = 0;
+        ways[0] = 1;
+        int mod = 1e9 + 7;
+
+        // Min-heap storing pairs of {distance, node}
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
+        pq.push({0, 0});
+
+        while (!pq.empty()) {
+            // Using C++17 structured binding for cleaner syntax
+            auto [current_dist, u] = pq.top(); 
+            pq.pop();
+
+            // Lazy deletion step
+            if (current_dist > dist[u]) continue;
+
+            for (const auto& [v, time] : adj[u]) {
+                if (dist[v] > current_dist + time) {
+                    // Strictly shorter path found
+                    dist[v] = current_dist + time;
+                    ways[v] = ways[u];
+                    pq.push({dist[v], v});
+                } else if (dist[v] == current_dist + time) {
+                    // Equal shortest path found
+                    ways[v] = (ways[v] + ways[u]) % mod;
                 }
             }
         }
-        return w[n - 1];
+
+        return ways[n - 1];
     }
 };
