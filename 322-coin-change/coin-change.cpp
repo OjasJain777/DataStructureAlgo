978e066d92b2a3e37f9af51005bbb0422ef8478d
OjasJain777 <ojasjain777@gmail.com>
2026-07-14 10:43:02 +0530

    Time: 35 ms (53.71%) | Memory: 19 MB (59.89%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,25 +1,27 @@
-int t(int target, int k, vector<vector<int>> &dp, vector<int> &coins){
-    if(target<0){
+int t(int target, vector<int> &dp, vector<int> &coins){
+    if(target <0){
         return 1e9;
     }
-    if(target == 0){
-        return dp[target][k] = 0;
+    if(target==0){
+        return dp[target] = 0;
     }
-    if(k<0){
-        return 1e9;
+    if(dp[target] != -1){
+        return dp[target];
     }
-    if(dp[target][k] != -1){
-        return dp[target][k];
+    dp[target] = 1e9;
+    for(int x: coins){
+        dp[target] = min(dp[target], t(target - x, dp, coins) + 1);
     }
-    int a = t(target - coins[k],k, dp, coins) +1;
-    int b = t(target, k-1, dp,coins);
-    return dp[target][k] = min(a,b);
+    return dp[target];
 }
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(amount+1, vector<int>(coins.size()+1,-1));
-        int a = t(amount,coins.size()-1, dp, coins);
-        return a >= 1e9 ? -1 : a;
+        vector<int> dp(amount+1, -1);
+        int a= t(amount, dp, coins);
+        if(a==1e9){
+            return -1;
+        }
+        return a;
     }
 };
