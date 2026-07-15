d07ef96aea5a2da27014c317eabae79475a00c05
OjasJain777 <ojasjain777@gmail.com>
2026-07-15 07:33:26 +0530

    Time: 55 ms (28.68%) | Memory: 29.1 MB (73.60%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -2,40 +2,37 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-
         vector<vector<int>> ans;
-        int n = nums.size();
-
-        for(int i = 0; i < n - 2; i++) {
-
-            if(i > 0 && nums[i] == nums[i - 1])
-                continue;
-
-            int l = i + 1;
-            int r = n - 1;
-
-            while(l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-
-                if(sum < 0) {
-                    l++;
+        for(int i = 0 ; i<nums.size();i++){
+            if(i!=0 && nums[i] == nums[i-1]){continue;}
+            int s= i+1;
+            int e  = nums.size()-1;
+            while(e>s){
+                if(nums[e]+nums[s]+nums[i]==0){
+                    ans.push_back({nums[i], nums[s], nums[e]});
+                    s++;
+                    e--;
+                    while(s < e && nums[s] == nums[s-1]){
+                        s++;
+                    }
+                    while(e>s && nums[e] == nums[e+1]){
+                        e--;
+                    }
                 }
-                else if(sum > 0) {
-                    r--;
+                else if(nums[s]+nums[e]+nums[i] < 0){
+                    s++;
+                    while(s < e && nums[s] == nums[s-1]){
+                        s++;
+                    }
                 }
-                else {
-                    ans.push_back({nums[i], nums[l], nums[r]});
-
-                    // Skip duplicates
-                    while(l < r && nums[l] == nums[l + 1]) l++;
-                    while(l < r && nums[r] == nums[r - 1]) r--;
-
-                    l++;
-                    r--;
+                else{
+                    e--;
+                    while(e>s && nums[e] == nums[e+1]){
+                        e--;
+                    }
                 }
             }
         }
-
         return ans;
     }
 };
