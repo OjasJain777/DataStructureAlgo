fcd82763ceb34c13556ebd3faf39d2785939d74f
OjasJain777 <ojasjain777@gmail.com>
2026-07-15 08:20:51 +0530

    Time: 33 ms (55.62%) | Memory: 23 MB (47.91%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,27 +1,34 @@
-int t(int target, vector<int> &dp, vector<int> &coins){
-    if(target <0){
-        return 1e9;
-    }
-    if(target==0){
-        return dp[target] = 0;
-    }
-    if(dp[target] != -1){
-        return dp[target];
-    }
-    dp[target] = 1e9;
-    for(int x: coins){
-        dp[target] = min(dp[target], t(target - x, dp, coins) + 1);
-    }
-    return dp[target];
-}
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1, -1);
-        int a= t(amount, dp, coins);
-        if(a==1e9){
-            return -1;
+        queue<int> q;
+        vector<int> vis(amount + 1, false);
+
+        q.push(0);
+        vis[0] = true;
+
+        int lvl = 0;
+
+        while (!q.empty()) {
+            int sz = q.size();
+
+            while (sz--) {
+                int x = q.front();
+                q.pop();
+
+                if (x == amount)
+                    return lvl;
+
+                for (int c : coins) {
+                    if (x <= amount - c && !vis[x + c]) {
+                        vis[x + c] = true;
+                        q.push(x + c);
+                    }
+                }
+            }
+            lvl++;
         }
-        return a;
+
+        return -1;
     }
 };
