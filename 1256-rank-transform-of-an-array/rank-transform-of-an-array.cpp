843033e5274d84491d2ca852427e507cec8c0be8
OjasJain777 <ojasjain777@gmail.com>
2026-07-17 21:46:09 +0530

    Time: 37 ms (55.90%) | Memory: 43.8 MB (58.86%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> a = arr;
+        sort(a.begin(),a.end());
+        unordered_map<int,int> m;
+        int r =1;
+        for(int i=0; i<a.size();i++){
+            if(m.count(a[i])!=0){
+                continue;
+            }
+            m[a[i]] = r;
+            r++;
+        }
+        for(int i = 0; i<arr.size();i++){
+            arr[i] = m[arr[i]];
+        }
+        return arr;
+    }
+};
