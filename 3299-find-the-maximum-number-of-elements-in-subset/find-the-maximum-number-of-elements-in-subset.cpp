dd102c3c1560636ad1bd44d55b74a7b66a4cb8ec
OjasJain777 <ojasjain777@gmail.com>
2026-07-30 18:48:29 +0530

    Time: 110 ms (27.77%) | Memory: 139 MB (5.73%) - LeetSync

--- 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.cpp
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        unordered_map<long long, int> m;
-        for (int x : nums) {
+        unordered_map<long long,int> m;
+        for(int x: nums){
             m[x]++;
         }
         int ans;
-        if (m[1] % 2 == 0) {
-            ans = m[1] - 1;
-        } else {
-            ans = m[1];
+        if(m[1]%2==0){
+            ans= m[1]-1;
+        }
+        else{
+            ans  = m[1];
         }
         long long a;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 1) {
+        for(int i =0; i<nums.size();i++){
+            if(nums[i]==1){
                 continue;
             }
             a = nums[i];
-            int c = 0;
-            while (a <= 1000000000LL && m[a] >= 2) {
-                c++;
-                a *= a;
+            int c  = 0;
+            while(a<1e9 && m[a]>=2){
+                c++;;
+                a = a*a;
             }
-
-            if (a <= 1000000000LL && m[a] == 1)
-                c = c * 2 + 1;
-            else
-                c = c * 2 - 1;
+            if(m[a]==1){
+                c *= 2;
+                c++;
+            }
+            else{
+                c = c*2 - 1;
+            }
             ans = max(ans, c);
         }
         return ans;
