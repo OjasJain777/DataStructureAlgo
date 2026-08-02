86c62fa52170d432c605c9c8123d04a6217a9057
OjasJain777 <ojasjain777@gmail.com>
2026-08-02 17:49:21 +0530

    Time: 4 ms (45.35%) | Memory: 25.9 MB (9.69%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,58 +1,25 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int i = 0;
-        while(i<board.size()){
-            unordered_map<char,int> fm;
-            for(int j =0;j<board[0].size();j++){
-                if(board[i][j] <= '9' && board[i][j] >='1'){
-                    if(fm[board[i][j]]!=0){
-                        return false;
-                    }
-                    else{
-                        fm[board[i][j]]++;
-                    }
-                }
+        vector<unordered_set<char>> row(9);
+        vector<unordered_set<char>> col(9);
+        vector<vector<unordered_set<char>>> box(3, vector<unordered_set<char>>(3));
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+
+                char x = board[i][j];
+
+                if (row[i].count(x) || col[j].count(x) || box[i / 3][j / 3].count(x))
+                    return false;
+
+                row[i].insert(x);
+                col[j].insert(x);
+                box[i / 3][j / 3].insert(x);
             }
-            i++;
         }
-        i = 0;
-        while(i<board[0].size()){
-            unordered_map<char,int> fm2;
-            for(int j =0; j<board.size();j++){
-                if(board[j][i]>='1' && board[j][i]<='9'){
-                    if(fm2[board[j][i]]!=0){
-                        return false;
-                    }
-                    else{
-                        fm2[board[j][i]]++;
-                    }
-                }
-            }
-            i++;
-        }
-        i = 0;
-        int j =0;
-        while(i<board.size()){
-            while(j<board[0].size()){
-                unordered_map<char,int> v;
-                for(int r = i;r<i+3;r++){
-                    for(int c = j;c<j+3;c++){
-                        if(board[r][c]<='9' && board[r][c]>='1'){
-                            if(v[board[r][c]]!=0){
-                                return false;
-                            }
-                            else{
-                                v[board[r][c]]++;
-                            }
-                        }
-                    }
-                }
-                j+=3;
-            }
-            j=0;
-            i+=3;
-        }
+
         return true;
     }
 };
