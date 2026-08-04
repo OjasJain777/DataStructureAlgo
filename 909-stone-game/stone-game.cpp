8f7ae037b242811bcbadd3b0bb9f0655ce036517
OjasJain777 <ojasjain777@gmail.com>
2026-08-04 15:07:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (69.94%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,23 +1,20 @@
-int t(int s, int e, int p, vector<int> &nums, vector<vector<vector<int>>> &dp){
-    if(s>e){
-        return 0;
-    }
-    if(dp[s][e][p] != INT_MIN){
-        return dp[s][e][p];
-    }
-    if(p){
-        dp[s][e][p] = max(t(s+1,e,0,nums, dp) + nums[s], t(s,e-1,0,nums,dp) + nums[e]);
-    }
-    else{
-        dp[s][e][p] = max(t(s+1,e,1,nums,dp) - nums[s] , t(s,e-1,1,nums,dp) - nums[e]);
-    }
-    return dp[s][e][p];
-}
 class Solution {
 public:
+    vector<vector<int>> dp;
+    int sol(int i , int j , vector<int> & nums){
+        if(i>j) return 0;
+        if(i==j)  return  nums[i];
+        if(dp[i][j]!=-1) return dp[i][j];
+        int first = nums[i]+ min(sol(i+2,j, nums),sol(i+1,j-1,nums));
+        int second = nums[j]+ min(sol(i,j-2, nums),sol(i+1,j-1,nums));
+        return  dp[i][j]=max(first,second);
+    }
     bool stoneGame(vector<int>& nums) {
-        vector<vector<vector<int>>> dp(nums.size(), vector<vector<int>> (nums.size(), vector<int>(2, INT_MIN)));
-        t(0, nums.size()-1, 1, nums,dp);
-        return dp[0][nums.size()-1][1] >0 ? 1 : 0;
+        return true;
+        int total=0;
+        for(int i : nums) total+=i;
+        dp.assign(501,vector<int> (501,-1));
+        int a= sol(0,nums.size()-1, nums);
+        return a>=(total-a);
     }
 };
