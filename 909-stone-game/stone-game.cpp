b291abbc408bbc7a30a66bd110adb8b225c009dc
OjasJain777 <ojasjain777@gmail.com>
2026-08-04 14:49:07 +0530

    Time: 19 ms (12.57%) | Memory: 19.7 MB (9.20%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,23 @@
+int t(int s, int e, int p, vector<int> &nums, vector<vector<int>> &dp){
+    if(s>e){
+        return 0;
+    }
+    if(dp[s][e] != INT_MIN){
+        return dp[s][e];
+    }
+    if(p){
+        dp[s][e] = max(t(s+1,e,0,nums, dp) + nums[s], t(s,e-1,0,nums,dp) + nums[e]);
+    }
+    else{
+        dp[s][e] = max(t(s+1,e,1,nums,dp) - nums[s] , t(s,e-1,1,nums,dp) - nums[e]);
+    }
+    return dp[s][e];
+}
+class Solution {
+public:
+    bool stoneGame(vector<int>& nums) {
+        vector<vector<int>> dp(nums.size(), vector<int> (nums.size(),INT_MIN));
+        t(0, nums.size()-1, 1, nums,dp);
+        return dp[0][nums.size()-1] >0 ? 1 : 0;
+    }
+};
