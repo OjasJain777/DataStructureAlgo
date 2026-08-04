a54607eedd23ab758d146059fccd0a276c3a605e
OjasJain777 <ojasjain777@gmail.com>
2026-08-04 14:36:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (22.07%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,23 +1,23 @@
-int t(int s, int e, int p, vector<int> &nums,vector<vector<vector<int>>> &dp){
+int t(int s, int e, int p, vector<int> &nums,vector<vector<int>> &dp){
     if(s>e){
         return 0;
     }
-    if(dp[s][e][p]!=INT_MIN){
-        return dp[s][e][p];
+    if(dp[s][e] !=INT_MIN){
+        return dp[s][e];
     }
     if(p){
-        dp[s][e][p] = max(nums[s] + t(s+1,e,0,nums,dp) , nums[e] + t(s,e-1,0,nums,dp));
+        dp[s][e] = max(nums[s] + t(s+1,e,0,nums,dp) , nums[e] + t(s,e-1,0,nums,dp));
     }
     else{
-        dp[s][e][p] = min( t(s+1,e,1,nums,dp) - nums[s] , t(s,e-1,1,nums,dp) - nums[e]);
+        dp[s][e] = min( t(s+1,e,1,nums,dp) - nums[s] , t(s,e-1,1,nums,dp) - nums[e]);
     }
-    return dp[s][e][p];
+    return dp[s][e];
 }
 class Solution {
 public:
     bool predictTheWinner(vector<int>& nums) {
-        vector<vector<vector<int>>> dp(nums.size(), vector<vector<int>>(nums.size(),vector<int>(2, INT_MIN)));
+        vector<vector<int>> dp(nums.size(), vector<int> (nums.size(),INT_MIN));
         t(0, nums.size()-1, 1,nums, dp);
-        return dp[0][nums.size()-1][1] >=0 ? 1 : 0;
+        return dp[0][nums.size()-1] >=0 ? 1 : 0;
     }
 };
