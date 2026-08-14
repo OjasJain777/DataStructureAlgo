161eb8177fbe48ae0fa29ca7332a0eeca062f091
OjasJain777 <ojasjain777@gmail.com>
2026-08-14 06:56:47 +0530

    Time: 6 ms (8.40%) | Memory: 14.3 MB (5.67%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.cpp
+++ 1095-two-city-scheduling/two-city-scheduling.cpp
@@ -1,28 +1,18 @@
-int t(int i, int k, vector<vector<int>> &c, vector<vector<int>> &dp){
-    if(i==0){
-        if(k== c.size()/2){
-            return c[0][1];
-        }
-        else if(k == c.size()/2-1){
-            return c[0][0];
-        }
-        return 1e9;
-    }
-    if(dp[i][k]!=-1){
-        return dp[i][k];
-    }
-    if(k== c.size()/2){
-        dp[i][k] = t(i-1, k, c,dp) + c[i][1];
-    }
-    else{
-        dp[i][k] = min(t(i-1, k+1, c, dp) + c[i][0] , t(i-1,k,c,dp)+c[i][1]);
-    }
-    return dp[i][k];
-}
 class Solution {
 public:
-    int twoCitySchedCost(vector<vector<int>>& costs) {
-        vector<vector<int>> dp(costs.size(), vector<int>(costs.size()/2+1,-1));
-        return t(costs.size()-1, 0, costs, dp);
+    int twoCitySchedCost(vector<vector<int>>& c) {
+        int ans =0;
+        sort(c.begin(), c.end(), [](vector<int> a, vector<int> b){
+            return a[0]-a[1] > b[0]-b[1];
+        });
+        for(int i =0; i<c.size();i++){
+            if(i<c.size()/2){
+                ans += c[i][1];
+            }
+            else{
+                ans += c[i][0];
+            }
+        }
+        return ans;
     }
 };
