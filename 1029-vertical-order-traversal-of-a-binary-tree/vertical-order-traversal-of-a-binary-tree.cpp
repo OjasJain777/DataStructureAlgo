7243ab5d00921f59a6b28e900857e42830dd1d17
OjasJain777 <ojasjain777@gmail.com>
2026-08-20 07:24:24 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (95.27%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,36 +1,44 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-void t(map<int, map<int, vector<int>>> &b, TreeNode *a, int i, int j){
-    if(a==0){
-        return;
-    }
-    b[j][i].push_back(a->val);
-    t(b, a->left, i+1, j-1);
-    t(b,a->right, i+1, j+1);
-}
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, vector<int>>> b;
-        t(b, root, 0,0);
-        vector<vector<int>> ans;
-        for(auto y: b){
-            vector<int> m;
-            for(auto x: y.second){
-                sort(x.second.begin(), x.second.end());
-                m.insert(m.end(), x.second.begin(), x.second.end());
+        map<int, vector<int>> m;
+        auto cmp = [](pair<TreeNode*, int>& a,
+                      pair<TreeNode*, int>& b) -> bool {
+            return a.first->val > b.first->val;
+        };
+        priority_queue<pair<TreeNode*, int>, vector<pair<TreeNode*, int>>,
+                       decltype(cmp)>
+            q(cmp);
+
+        q.push({root, 0});
+        priority_queue<pair<TreeNode*, int>, vector<pair<TreeNode*, int>>,
+                       decltype(cmp)>
+            q2(cmp);
+        while (!q.empty()) {
+            auto [t, l] = q.top();
+            q.pop();
+
+            m[l].push_back(t->val);
+
+            if (t->left != 0) {
+                q2.push({t->left, l - 1});
             }
-            ans.push_back(m);
+
+            if (t->right != 0) {
+                q2.push({t->right, l + 1});
+            }
+
+            if (q.empty()) {
+                q = q2;
+                q2 = {};
+            }
         }
+
+        vector<vector<int>> ans;
+        for (auto& y : m) {
+            ans.push_back(y.second);
+        }
+
         return ans;
     }
 };
