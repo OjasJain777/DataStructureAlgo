26c1a163d7207b03d73b02ed79cb5c82db2d7a68
OjasJain777 <ojasjain777@gmail.com>
2026-08-20 10:07:45 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (72.15%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -9,24 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-bool t(TreeNode *a, TreeNode *b){
+bool t(TreeNode *a , TreeNode *b){
     if(a==0 && b==0){
         return 1;
     }
-    if(a==0 || b==0){
+    else if(a==0 || b==0){
         return 0;
     }
-    if(a->val !=b->val){
-        return 0;
+    if(a==b){
+        return t(a->left, b->right);
     }
-    return (t(a->left, b->right) && t(a->right, b->left));
+    else{
+        if(a->val == b->val){
+            bool l = t(a->left, b->right);
+            bool r = t(a->right, b->left);
+            return l && r;
+        }
+        else{
+            return 0;
+        }
+    }
+    return 0;
 }
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        TreeNode *a = root;
-        TreeNode *b = root;
-        return t(a, b);
-        
+        return t(root, root);
     }
 };
